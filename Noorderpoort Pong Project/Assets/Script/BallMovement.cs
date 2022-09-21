74b4968f49bfc2aab5f3ceba7c579e67b1d763ec
Niek <nmelet@st.noorderpoort.nl>
2022-09-21 15:47:55 +0200

    SnapShot Pong

--- Noorderpoort Pong Project/Assets/Script/BallMovement.cs
+++ Noorderpoort Pong Project/Assets/Script/BallMovement.cs
@@ -6,7 +6,6 @@ public class BallMovement : MonoBehaviour
     public float BallForce = 500f;
     Vector2 MyVelocity = new Vector2(1, 1);
 
-
     // Start is called before the first frame update
     void Start()
     {
