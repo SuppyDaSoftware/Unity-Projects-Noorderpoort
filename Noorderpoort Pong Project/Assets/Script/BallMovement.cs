071b2e15ffe50b070a708a80ec496a4e9aecca55
Niek <nmelet@st.noorderpoort.nl>
2022-09-22 13:21:19 +0200

    New SnapShot Pong

--- Noorderpoort Pong Project/Assets/Script/BallMovement.cs
+++ Noorderpoort Pong Project/Assets/Script/BallMovement.cs
@@ -3,20 +3,31 @@ using UnityEngine.Jobs;
 
 public class BallMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float BallForce = 500f;
-    Vector2 MyVelocity = new Vector2(1, 1);
+    public float moveSpeed= 4f;
+    private Vector2 direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb.GetComponent<Rigidbody2D>();
-        rb.AddForce(MyVelocity * BallForce);
+        direction = new Vector2(Random.Range(-1f,1f), Random.Range(-1f,1f));
+        direction = direction.normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction = Vector2.Reflect(direction, collision.contacts[0].normal);
+        }
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            direction = Vector2.Reflect(direction, collision.contacts[0].normal);
+        }
     }
 }
