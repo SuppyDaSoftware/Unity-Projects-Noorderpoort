b1837e686d3528cf0f743934bbb40651cf4f949f
Niek <nmelet@st.noorderpoort.nl>
2022-09-28 15:32:20 +0200

    Recovery Pong

---
+++ Noorderpoort Pong Project/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Variables
+    public int playerNumber = 1;
+    public int playerSpeed = 7;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player collision movement to stop when it hits the border
+        
+
+        //Player 1 movement
+        if (playerNumber == 1)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxis("Player1") * Time.deltaTime * playerSpeed));
+        }
+        //Player 2 movement
+        if (playerNumber == 2)
+        {
+            transform.Translate(new Vector3(0, Input.GetAxis("Player2") * Time.deltaTime * playerSpeed));
+        }
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, -4.28f, 4.28f));
+
+    }
+}
