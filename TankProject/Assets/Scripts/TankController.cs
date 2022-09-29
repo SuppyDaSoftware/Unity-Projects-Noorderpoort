6a6f8747063f14c36ac59261f4ea32280f2ec14e
Niek <nmelet@st.noorderpoort.nl>
2022-09-29 14:34:58 +0200

    Added bullet fire script
    pog

--- TankProject/Assets/Scripts/TankController.cs
+++ TankProject/Assets/Scripts/TankController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
+    [SerializeField]
+    Transform barrelRotator;
+    [SerializeField]
+    Transform firePoint;
+    [SerializeField]
+    GameObject bulletToFire;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Rotate Barrel
+        barrelRotator.RotateAround(Vector3.forward, Input.GetAxis("Vertical")* Time.deltaTime);
+
+        //Fire Bullet
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+        }
     }
 }
