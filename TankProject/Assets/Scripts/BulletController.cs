d2b24b7846f07ebc87809a794543f182bdb866e5
Niek <nmelet@st.noorderpoort.nl>
2022-09-29 14:04:04 +0200

    Added some Scripts and Prototype Models
    Bullet script and prefab

---
+++ TankProject/Assets/Scripts/BulletController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    //Bullet Time to live
+   public float bulletTtl = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletTtl -= Time.deltaTime;
+        if (bulletTtl <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy (gameObject);
+    }
+}
