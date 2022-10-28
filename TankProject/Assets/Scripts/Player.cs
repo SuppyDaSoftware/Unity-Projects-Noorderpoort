fafa6f253b00d8d041326d6790089581f1edb23d
Niek Melet <nmelet@st.noorderpoort.nl>
2022-10-29 01:30:50 +0200

    Added Scoreboard System

--- TankProject/Assets/Scripts/Player.cs
+++ TankProject/Assets/Scripts/Player.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int playerNumber;
-    public int maxHealth = 100;
-    public int currentHealth;
-
     bool isPlayerTurn = false;
-
-    public Healthbar healthbar;
-
     void Start()
     {
-       // currentHealth = maxHealth;
-       // healthbar.SetMaxHealth(maxHealth);
+
     }
 
     // Update is called once per frame
@@ -23,17 +16,10 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) && isPlayerTurn == true)
         {
-            takeDamage(20);
             Invoke("SwapTurn", 0.1f);
         }
     }
 
-    void takeDamage(int damage)
-    {
-        currentHealth -= damage;
-
-        //healthbar.SetHealth(currentHealth);
-    }
     public void SwapTurn()
     {
         GameObject.Find("GameManager").GetComponent<TurnManager>().SwapTurn();
